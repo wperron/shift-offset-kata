39f60fe014da5b43e1cd255c076805e3215f7e87
wperron <william.perron@live.ca>
2020-03-03 21:19:06 -0500

    fixed edge case where the input list has not been shifted

--- shift_offset.go
+++ shift_offset.go
@@ -9,19 +9,23 @@ func IntOffset(list []int) int {
 		return 0
 	}
 
-	mid := int(math.Floor(float64(len(list))) / 2) - 1
+	if list[0] < list[len(list) - 1] {
+		return 0
+	}
+
+	mid := int(len(list) / 2)
 	low := 0
 	high := len(list) - 1
 	for low <= mid {
 		// check which side of the slice contains the shift
-		if list[low] > list[mid] {
+		if list[low] > list[mid - 1] {
 			high = mid
 			mid = low + int(math.Floor(float64(mid - low)) / 2)
-		} else if list[mid + 1] > list[high] {
-			low = mid + 1
+		} else if list[mid] > list[high] {
+			low = mid
 			mid = int(mid + ((high - mid) / 2))
 		} else {
-			return mid + 1
+			return mid
 		}
 	}
 	return mid
--- shift_offset_test.go
+++ shift_offset_test.go
@@ -13,6 +13,7 @@ func TestOffset(t *testing.T) {
 		{[]int{}, 0},
 		{[]int{2}, 0},
 		{[]int{3, 1}, 1},
+		{[]int{1, 2, 3}, 0},
 		{[]int{5, 7, 9, 1, 3, 4}, 3},
 		{[]int{5, 7, 9, 17, 25, 1, 3, 4}, 5},
 		{[]int{5, 7, 9, 17, 25, 31, 1, 3, 4}, 6},
@@ -22,7 +23,7 @@ func TestOffset(t *testing.T) {
 	for _, param := range params {
 		res = IntOffset(param.input)
 		if res != param.expected {
-			t.Errorf("Invalid offset, expected %d got %d\n", param.expected, res)
+			t.Errorf("Invalid offset, given %v, expected %d got %d\n", param.input, param.expected, res)
 		}
 	}
 }
